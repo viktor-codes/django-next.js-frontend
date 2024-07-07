7076d4e658248f186e3a44c78a4e1c8102378a05
Viktor Rura <ruravictor@gmail.com>
2024-07-07 22:03:21 +0100

    Fix the login func

--- src/app/api/login/route.jsx
+++ src/app/api/login/route.jsx
@@ -1,8 +1,12 @@
 "use server"
 
-import {cookies} from "next/headers"
 import { NextResponse } from "next/server"
 
-export async function Page(request) {
+
+export async function POST(request) {
+
+    const data = await request.json()
+    console.log(data)
+    
     return NextResponse.json({message: "Hello"}, {status: 200})
 }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,15 +2,16 @@
 
 // import { cookies } from "next/headers"
 
-const LOGIN_URL = "http://127.0.0.1:8001/api/token/pair"
+
+const LOGIN_URL = "/api/login/"
 
 
 export default function Page(){
-    async function handleSubmit(event){
+    async function handleSubmit (event) {
         event.preventDefault()
         console.log(event, event.target)
         const formData = new FormData(event.target)
-        const objectFromForm = Object.fromEntries(formData) 
+        const objectFromForm = Object.fromEntries(formData)
         const jsonData = JSON.stringify(objectFromForm)
         const requestOptions = {
             method: "POST",
@@ -18,25 +19,32 @@ export default function Page(){
                 "Content-Type": "application/json"
             },
             body: jsonData
-            }
-            const response = await fetch(LOGIN_URL, requestOptions)
-            const data = await response.json()
-            console.log(data)
-            if (response.ok){
-                console.log("logged in")
-
-        } 
-
+        }
+        const response = await fetch(LOGIN_URL, requestOptions)
+        let data = {}
+        try {
+          data = await response.json()
+        } catch (error) {
+          
+        }
+        // const data = await response.json()
+        if (response.ok) {
+            console.log("logged in")
+        } else {
+          console.log(await response.json())
+        }
     }
+
+    
 
     return (
         <div className="h-[95vh]">
             <div className="max-w-md mx-auto py-5">
                 <h1>Login Here</h1>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" name="username" 
+                    <input className="text-black" type="text" name="username" 
                     placeholder="Your Username" required/>
-                    <input type="text" name="password" 
+                    <input className="text-black" type="password" name="password" 
                     placeholder="Your Password" required/>
                     <button type="submit">login</button>
                 </form>
